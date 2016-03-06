40258fcdc876e66ffb23e8ac1fcc5fee4ef36ae5
Bhargav Srinivasan <bhargavsvasan@hotmail.com>
2016-03-06 16:40:12 -0500

    Restructuring v0.6

--- src/com/chessbsv/Bishop.java
+++ src/com/chessbsv/Bishop.java
@@ -1,7 +1,11 @@
 package com.chessbsv;
 
+import com.sun.org.apache.xpath.internal.functions.FuncFalse;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Created by bsriniva on 2/21/16.
@@ -13,25 +17,38 @@ public class Bishop extends Piece {
     piece all the way to the ends of the board irrespective of other pieces on the board.
     */
 
-    ArrayList<Square> possibleMovesDiagonal1 = new ArrayList<>();
-    ArrayList<Square> possibleMovesDiagonal2 = new ArrayList<>();
-    ArrayList<Square> possibleMovesDiagonal3 = new ArrayList<>();
-    ArrayList<Square> possibleMovesDiagonal4 = new ArrayList<>();
+    ArrayList<Integer> possibleMovesDiagonal1 = new ArrayList<>();
+    ArrayList<Integer> possibleMovesDiagonal2 = new ArrayList<>();
+    ArrayList<Integer> possibleMovesDiagonal3 = new ArrayList<>();
+    ArrayList<Integer> possibleMovesDiagonal4 = new ArrayList<>();
 
     /*
     Diagonals considering blocking pieces, filled in by Bishop_rules,
     Considering the current state of the board.
     */
 
-    ArrayList<Square> allowedMovesDiagonal1 = new ArrayList<>();
-    ArrayList<Square> allowedMovesDiagonal2 = new ArrayList<>();
-    ArrayList<Square> allowedMovesDiagonal3 = new ArrayList<>();
-    ArrayList<Square> allowedMovesDiagonal4 = new ArrayList<>();
+    ArrayList<Integer> allowedMovesDiagonal1 = new ArrayList<>();
+    ArrayList<Integer> allowedMovesDiagonal2 = new ArrayList<>();
+    ArrayList<Integer> allowedMovesDiagonal3 = new ArrayList<>();
+    ArrayList<Integer> allowedMovesDiagonal4 = new ArrayList<>();
+
+    /*
+    Boundary Hashes to determine out of bounds check, while
+    populating the diagonals.
+    */
+
+    HashSet<Integer> topLeft = new HashSet<>();
 
 
-    Bishop(String xID, String currentPos){
+    Bishop(String xID, String currentPos,piece_color color,piece_type type) throws IllegalArgumentException {
         this.xID = xID;
-        this.currentPos = currentPos;
+        this.color = color;
+        this.type = type;
+        if(this.validPosition(currentPos)){
+            this.currentPos = currentPos;
+        } else {
+            throw new IllegalArgumentException("Invalid Bishop position");
+        }
     }
 
     public void dispMove(String newPosition){
@@ -40,6 +57,7 @@ public class Bishop extends Piece {
         String curPos = this.currentPos;
         int cPos = this.positionResolver(curPos);
         int newPos = this.positionResolver(newPosition);
+        possibleMoves(cPos,newPos);
     }
 
 
@@ -47,7 +65,12 @@ public class Bishop extends Piece {
         //public int populate_diagonal();
         // check diagonal entries, store aLL hashed square ids along 4 1D arrays.
         System.out.println("Current Pos: "+currentPos+" "+newPosition);
+        String XIDres = this.xIDResolver(currentPos);
+        System.out.println("Current xID: "+XIDres);
 
+        //while (!topLeft.contains(currentPos)){
+
+        //}
         //while index !in (starting col && starting row)
         //pushback currpos-9
         //end
@@ -66,7 +89,9 @@ public class Bishop extends Piece {
     public String toString() {
         StringBuffer buf = new StringBuffer();
         buf.append("ID: "+xID+"\n");
-        buf.append("Current Position: "+xID+"\n");
+        buf.append("Piece type: "+type+"\n");
+        buf.append("Piece color: "+color+"\n");
+        buf.append("Current Position: "+currentPos+"\n");
         return buf.toString();
     }
 
--- src/com/chessbsv/Piece.java
+++ src/com/chessbsv/Piece.java
@@ -9,31 +9,84 @@ import java.util.HashMap;
 public abstract class Piece {
 
     String xID;
+    public enum piece_color{BLACK,WHITE}
+    public enum piece_type{BISHOP,KING,KNIGHT,PAWN,ROOK,QUEEN}
+    piece_color color;
+    piece_type type;
     String currentPos;
 
     public abstract void dispMove(String d1);
 
     HashMap<String,Integer> alphaNum = new HashMap<>();
+    HashMap<Integer,String> numAlpha = new HashMap<>();
 
     public int positionResolver(String pos){
+
         loadAlphaNum();
 
         int rowNumber = alphaNum.get(pos.substring(0,1));
         int colNumber = Integer.decode(String.valueOf(pos.charAt(1)));
-        System.out.println("Row: "+rowNumber+"Col: "+colNumber);
+        //System.out.println("Row: "+rowNumber+"Col: "+colNumber);
         int boardIndex = (rowNumber-1)*8 + (colNumber-1);
         return boardIndex;
 
     }
 
+    public String xIDResolver(int pos){
+
+        loadNumAlpha();
+        Integer colNumber = (pos%8) + 1;
+        Integer rowNumber = (pos/8) + 1;
+
+        StringBuffer ID = new StringBuffer();
+        ID.append(numAlpha.get(rowNumber.intValue()));
+        ID.append(colNumber.toString());
+
+        System.out.println("Row: "+rowNumber+"Col: "+colNumber+"Pos: "+pos);
+
+        return ID.toString();
+    }
+
+    public boolean validPosition(String currentPos){
+        int pos = this.positionResolver(currentPos);
+        if(pos<0 || pos>=64){
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    public boolean validPosition(int currentPos){
+        int pos = currentPos;
+        if(pos<0 || pos>=64){
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
     public void loadAlphaNum(){
-        alphaNum.put("A",0);
-        alphaNum.put("B",1);
+        alphaNum.put("A",1);
+        alphaNum.put("B",2);
         alphaNum.put("C",3);
         alphaNum.put("D",4);
         alphaNum.put("E",5);
         alphaNum.put("F",6);
         alphaNum.put("G",7);
+        alphaNum.put("H",8);
+    }
+
+    public void loadNumAlpha(){
+        numAlpha.put(1,"A");
+        numAlpha.put(2,"B");
+        numAlpha.put(3,"C");
+        numAlpha.put(4,"D");
+        numAlpha.put(5,"E");
+        numAlpha.put(6,"F");
+        numAlpha.put(7,"G");
+        numAlpha.put(8,"H");
     }
 
 
